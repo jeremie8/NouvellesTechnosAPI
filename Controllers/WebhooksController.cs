8a645bc506c21c80a36b6f40fe1efe607851d4ff
Jérémie Vézina <jeremie.vezina@hotmail.com>
2020-05-23 23:35:40 -0400

    frist

---
+++ Controllers/WebhooksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NouvellesTechnosAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WebhooksController : ControllerBase
+	{
+		private static Message LastMessage = new Message() { Content = "" };
+
+		[HttpGet]
+		public string Get()
+		{
+			return LastMessage.Content;
+		}
+
+		[HttpPost]
+		public void Post([FromBody] Message message)
+		{
+			LastMessage = message;
+		}
+	}
+}
---
+++ Message.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace NouvellesTechnosAPI
+{
+	public class Message
+	{
+		public string Content { get; set; }
+	}
+}
